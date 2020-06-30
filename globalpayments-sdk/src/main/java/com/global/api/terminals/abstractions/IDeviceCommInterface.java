3bff6ea0daa83b5c8649c78f6a413c39fb50ed93
peter-yanong <peteryanongwork@gmail.com>
2020-06-30 19:13:31 +0800

    Remove setMessageReceivedHandler declaration in IDeviceCommInterface

--- globalpayments-sdk/src/main/java/com/global/api/terminals/abstractions/IDeviceCommInterface.java
+++ globalpayments-sdk/src/main/java/com/global/api/terminals/abstractions/IDeviceCommInterface.java
@@ -3,14 +3,16 @@ package com.global.api.terminals.abstractions;
 import com.global.api.entities.exceptions.ApiException;
 import com.global.api.entities.exceptions.ConfigurationException;
 import com.global.api.terminals.messaging.IBroadcastMessageInterface;
-import com.global.api.terminals.messaging.IMessageReceivedInterface;
 import com.global.api.terminals.messaging.IMessageSentInterface;
 
 public interface IDeviceCommInterface {
     void connect() throws ConfigurationException;
+
     void disconnect();
+
     byte[] send(IDeviceMessage message) throws ApiException;
+
     void setMessageSentHandler(IMessageSentInterface messageInterface);
+
     void setBroadcastMessageHandler(IBroadcastMessageInterface broadcastInterface);
-    void setMessageReceivedHandler(IMessageReceivedInterface messageReceivedInterface);
 }
