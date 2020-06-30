63998fa5e841cd0abab2decc9a83fca32342fdc6
peter-yanong <peteryanongwork@gmail.com>
2020-06-30 19:26:15 +0800

    Remove getReportData method

--- globalpayments-sdk/src/main/java/com/global/api/terminals/ingenico/responses/IngenicoTerminalReportResponse.java
+++ globalpayments-sdk/src/main/java/com/global/api/terminals/ingenico/responses/IngenicoTerminalReportResponse.java
@@ -1,32 +1,20 @@
 package com.global.api.terminals.ingenico.responses;
 
-import androidx.annotation.NonNull;
-
-import com.global.api.terminals.TerminalUtilities;
 import com.global.api.terminals.abstractions.ITerminalReport;
 
-public class IngenicoTerminalReportResponse extends com.global.api.terminals.ingenico.responses.IngenicoBaseResponse implements ITerminalReport {
-    private byte[] buffer;
-    private String _reportData;
+import java.nio.charset.StandardCharsets;
+
+public class IngenicoTerminalReportResponse extends IngenicoTerminalResponse implements ITerminalReport {
 
     public IngenicoTerminalReportResponse(byte[] buffer) {
-        this.buffer = buffer;
-        _reportData = this.buffer.toString();
-        String status = this.buffer.length > 0 ? "SUCCESS" : "FAILED";
-        setStatus(status);
+        super(buffer);
+        parseResponse(buffer);
     }
 
-    public String getReportData() {
-        return this._reportData;
-    }
-
-    public void setReportData(String reportData) {
-        this._reportData = reportData;
-    }
-
-    @NonNull
     @Override
-    public String toString() {
-        return TerminalUtilities.getString(buffer);
+    public void parseResponse(byte[] response) {
+        super.parseResponse(response);
+        rawData = new String(response, StandardCharsets.UTF_8);
+        privateData = rawData.substring(70);
     }
 }
